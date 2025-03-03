f118c65cea652a5140939f6e6cf85427e8f5d414
Dwayne <dune.njenga@student.moringaschool.com>
2025-03-03 10:53:56 +0300

    Added Array Functions

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis','Garfield']
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+    return [...cats, name]; 
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+    return[name, ...cats];
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+    return[...cats]
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+    return[...cats];
+}
+
+function appendCat(name){
+    return [...cats, "Broom"];
+}
+
+function prependCat(name){
+    return ["Arnold", ...cats ]
+}
+
+function removeLastCat(){
+    return cats.slice(0, 2)
+}
+function removeFirstCat(){
+    return cats.slice(1);
+}
+
